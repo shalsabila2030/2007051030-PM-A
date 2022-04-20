ab7e7e5f3ac7f92a31916fe86b5e26f5d4e4c1b8
shalsabila2030 <shalsa.bila2030@students.unila.ac.id>
2022-04-20 22:17:42 +0700

    (LKP 9) Navigation Drawer

---
+++ 2007051030_Shalsa Bila_LKP 9 Navigation Drawer/app/src/main/java/com/example/navigationdrawer/ui/gallery/GalleryFragment.java
@@ -0,0 +1,26 @@
+package com.example.navigationdrawer.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.navigationdrawer.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+}
+
---
+++ 2007051030_Shalsa Bila_LKP 9 Navigation Drawer/app/src/main/java/com/example/navigationdrawer/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.navigationdrawer.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.navigationdrawer.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+}
