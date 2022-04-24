5465a90d79f1eb9e97a2043c505d8987dbc285b1
shalsabila2030 <shalsa.bila2030@students.unila.ac.id>
2022-04-25 04:04:09 +0700

    (LKP 10) Tab Layout

---
+++ 2007051030_Shalsa Bila_LKP 10 Tab Layout/app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment1(),"Chat");
+        vpAdapter.addFragment(new fragment2(), "Status");
+        vpAdapter.addFragment(new fragment3(), "Panggilan");
+        viewPager.setAdapter(vpAdapter);
+    }
+}
